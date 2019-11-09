a2a7533390fd918bbb30f824a3d5b76ba1aac7ec
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-09 20:37:11 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,6 +1,17 @@
 class Cat
   
+  attr_accessor :Owner, :mood
+  attr_reader :name
   
+  @@all = []
   
+  def initialize(name,Owner)
+    @name = name
+    @mood = "nervous"
+    @@all << self 
+  end 
   
+  def all 
+    @@all 
+  end 
 end
