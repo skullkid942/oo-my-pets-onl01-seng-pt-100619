f27308883644875d70a1b2b75215bf5873322b7d
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-09 20:21:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,9 +2,29 @@ class Owner
   
   attr_reader :name, :species
   
-  def initialize(name, species = "human")
-    @name = name 
+  @@all = []
+  
+  def initialize(name, species)
+    @name = name
+    @species = "human"
+    @@all << self 
   end
+  
+  def say_species
+    puts "I am #{@species}."
+  end 
+  
+  def all 
+    @@all 
+  end
+  
+  def count
+    Owner.all.count
+  end 
+  
+  def reset_all
+    @@all.clear
+  end 
   
   
 end
